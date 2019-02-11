5042cbb4208e8febeb31cec2eb8d540edeabe565
Chua Swee Chin <chuasweechin@Chuas-MacBook-Air.local>
2019-02-11 17:56:04 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,18 @@
-console.log("Up and running!");
+var cardsInPlay = [];
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardFour);
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
